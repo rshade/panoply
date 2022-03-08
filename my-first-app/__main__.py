1d53aa2d8278f16c193654f8deb8661114c4bdf2
Richard Shade <rshade98@hotmail.com>
2022-03-08 11:50:11 -0600

    Pulumi Learning

---
+++ my-first-app/__main__.py
@@ -0,0 +1,25 @@
+"""A Python Pulumi program"""
+
+import os
+import pulumi
+import pulumi_docker as docker
+
+stack = pulumi.get_stack()
+
+# build our backend image!
+backend_image_name = "backend"
+backend = docker.Image(backend_image_name,
+                        build=docker.DockerBuild(context=f"{os.getcwd()}/app/backend"),
+                        image_name=f"{backend_image_name}:{stack}",
+                        skip_push=True
+                        )
+
+frontend_image_name = "frontend"
+frontend = docker.Image(frontend_image_name,
+                        build=docker.DockerBuild(context=f"{os.getcwd()}/app/frontend"),
+                        image_name=f"{frontend_image_name}:{stack}",
+                        skip_push=True
+                        )
+
+# build our mongodb image!
+mongo_image = docker.RemoteImage("mongo", name="mongo:bionic")
